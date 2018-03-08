58991a03a70a4f02a81f85a66f7afb3ec9ebf6c9
Jordan Sanders <jordan.sanders@mymhc.ca>
2018-03-08 08:26:17 -0700

    AddedGarbage

--- ElectionTabulatorFPA/Master.Master.designer.cs
+++ ElectionTabulatorFPA/Master.Master.designer.cs
@@ -38,5 +38,14 @@ namespace ElectionTabulatorFPA {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
     }
 }
